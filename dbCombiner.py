41c55a6159339496b7fa965ccbf409f6dd362605
elu2 <60558250+elu2@users.noreply.github.com>
2021-06-06 18:21:24 -0700

    Incorporated GO bio processes.

--- dbCombiner.py
+++ dbCombiner.py
@@ -2,15 +2,17 @@ import pandas as pd
 import pickle
 
 # Read in pubchem db file with new col names
-pubchem_df = pd.read_pickle("./databases/pubchemdb.tsv", encoding="utf-8", sep="\t", header=None)
-chebi_df = pd.read_csv("./databases/chebidb.tsv", encoding="utf-8", sep="\t", header=None)
+pubchem_df = pd.read_csv("./databases/pubchemdb.tsv", encoding="utf-8", sep="\t", header=None).rename({0:"name", 1:"id"}, axis=1)
+chebi_df = pd.read_csv("./databases/chebidb.tsv", encoding="utf-8", sep="\t", header=None).rename({0:"name", 1:"id"}, axis=1)
 proteins0f_df = pd.read_csv("./databases/proteins0f.tsv", encoding="utf-8", sep="\t", header=None).rename({0:"name", 1:"id"}, axis=1).drop(2, axis=1).drop_duplicates().reset_index(drop=True)
 proteinsgp_df = pd.read_csv("./databases/proteinsgp.tsv", encoding="utf-8", sep="\t", header=None).rename({0:"name", 1:"id"}, axis=1).drop(2, axis=1).drop_duplicates().reset_index(drop=True)
 proteinsqz_df = pd.read_csv("./databases/proteinsqz.tsv", encoding="utf-8", sep="\t", header=None).rename({0:"name", 1:"id"}, axis=1).drop(2, axis=1).drop_duplicates().reset_index(drop=True)
+mesh_df = pd.read_csv("./databases/meshdb.tsv", encoding="utf-8", sep="\t", header=None).rename({0:"name", 1:"id"}, axis=1)
+gobioproc_df = pd.read_csv("./databases/gobioproc.tsv", encoding="utf-8", sep="\t", header=None).rename({0:"name", 1:"id"}, axis=1).drop([2, 3], axis=1).drop_duplicates().reset_index(drop=True)
 
 
 # Adds pubchem tag to ids
-# For some reason CHEBI is already tagged
+# For some reason CHEBI and GO is already tagged
 def tagger(spec_id, db_name):
     return str(db_name + str(spec_id))
 
@@ -19,9 +21,13 @@ pubchem_df["id"] = pubchem_df.id.apply(tagger, db_name="pubchem:")
 proteins0f_df["id"] = proteins0f_df.id.apply(tagger,db_name="uniprot:")
 proteinsgp_df["id"] = proteinsgp_df.id.apply(tagger,db_name="uniprot:")
 proteinsqz_df["id"] = proteinsqz_df.id.apply(tagger,db_name="uniprot:")
+mesh_df["id"] = mesh_df.id.apply(tagger, db_name="mesh:")
+
 
 full_df = pubchem_df
+full_df = full_df.append(mesh_df)
 full_df = full_df.append(chebi_df)
+full_df = full_df.append(gobioproc_df)
 full_df = full_df.append(proteins0f_df)
 full_df = full_df.append(proteinsgp_df)
 full_df = full_df.append(proteinsqz_df).reset_index(drop=True)
@@ -29,4 +35,4 @@ full_df = full_df.append(proteinsqz_df).reset_index(drop=True)
 # Turns all names into lower case
 full_df["name"] = full_df.name.apply(lambda a: str(a).lower())
 
-pickle.dump(full_df, open("combinedDBs.pkl", "wb"))
+pickle.dump(full_df, open("./databases/combinedDBs.pkl", "wb"))
