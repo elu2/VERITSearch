9385d4e87b5c668bd554c2692095b3298f6b860e
elu2 <60558250+elu2@users.noreply.github.com>
2021-06-06 18:22:01 -0700

    Delete databases/.ipynb_checkpoints directory

--- databases/.ipynb_checkpoints/pickler-checkpoint.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import pickle
-
-
-def pickler(to_pickle):
-    to_pickle_csv = pd.read_csv(to_pickle, encoding="utf-8", sep="\t", header=None).rename({0:"name", 1:"id"}, axis=1)
-    pickle.dump(to_pickle_csv, open(f"{to_pickle.split('.')[0]}.pkl", "wb"))
-    print(f"Pickled {to_pickle.split('.')[0]}")
-
